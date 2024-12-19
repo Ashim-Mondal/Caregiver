7647e1d8f1251c7c45dd89385c3c2d7fd20e5c27
Ashim mondal <128243831+Ashim-Mondal@users.noreply.github.com>
2024-12-19 16:24:29 +0530

    Delete Caregiver directory

--- Caregiver/Nurse/Payment Gateway/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('BASE_URL','http://localhost/Ashim/Caregiver/Nurse/Payment%20Gateway/');
-define('API_KEY','rzp_test_ZgNb5waRWgX3hj');
-define('API_SECRET','id1BSo3mZVzHqFTI2YitsaQb');
-define('COMPANY_NAME','Caregiver');
-
-
-?>
